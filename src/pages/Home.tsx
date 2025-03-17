557c143573f6d89cb0187f9b4b865a76ed8cb3ac
elena7993 <elenalee7993@gmail.com>
2025-03-17 20:19:59 +0900

    타입스크립트 시작

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Box
+      maxW={"450px"}
+      w={"100%"}
+      margin={"0 auto"}
+      bgColor={"white"}
+      minH={"100vh"}
+      h={"100%"}
+    >
+      Box
+    </Box>
+  );
+};
+
+export default Home;
